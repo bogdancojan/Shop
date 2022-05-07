4c4cfee6bc52309fb7cc8b993c8bf083ba5a89c5
bogdancojan <Bogdan.Cojan@theaccessgroup.com>
2022-05-07 18:23:27 +0300

    Gmail in development

--- rails/config/environments/development.rb
+++ rails/config/environments/development.rb
@@ -69,4 +69,18 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.delivery_method = :smtp
+  host = 'localhost:3000'
+  config.action_mailer.default_url_options = { :host => 'localhost:3000', protocol: 'http' }
+
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => "tt1397894@gmail.com",
+    :password             => "ttest231.qE",
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
 end
