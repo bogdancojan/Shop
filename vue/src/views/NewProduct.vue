485a37a2ac792a41a8f6055ce00c42f524221c86
bogdancojan <Bogdan.Cojan@theaccessgroup.com>
2022-05-20 19:18:34 +0300

    Vuelidate
    Validations for the new product form

--- vue/src/views/NewProduct.vue
+++ vue/src/views/NewProduct.vue
@@ -9,16 +9,28 @@
       <div class="mb-3">
         <label>Name</label><br />
         <input type="text" v-model="product.name" id="input_text" />
+        <div style="color: #ef233c" v-if="v$.product.name.$error">
+          <i class="fa-solid fa-triangle-exclamation"></i>
+          {{ v$.product.name.$errors[0].$message }}
+        </div>
       </div>
 
       <div class="mb-3">
         <label>Price (RON)</label><br />
         <input type="text" v-model="product.price" id="input_text" />
+        <div style="color: #ef233c" v-if="v$.product.price.$error">
+          <i class="fa-solid fa-triangle-exclamation"></i>
+          {{ v$.product.price.$errors[0].$message }}
+        </div>
       </div>
 
       <div class="mb-3">
         <label>Photo URL</label><br />
         <input type="text" v-model="product.photo_url" id="input_text" />
+        <div style="color: #ef233c" v-if="v$.product.photo_url.$error">
+          <i class="fa-solid fa-triangle-exclamation"></i>
+          {{ v$.product.photo_url.$errors[0].$message }}
+        </div>
       </div>
 
       <button @click="onSubmit()" id="submitBtn" type="button">
@@ -30,9 +42,16 @@
 
 <script>
 import axios from "axios";
+import useValidate from "@vuelidate/core";
+import { required, minLength, numeric, url } from "@vuelidate/validators";
 
 export default {
   name: "NewProduct",
+  setup() {
+    return {
+      v$: useValidate(),
+    };
+  },
   data() {
     return {
       product: {
@@ -42,15 +61,30 @@ export default {
       },
     };
   },
+  validations() {
+    return {
+      product: {
+        name: { required, minLength: minLength(5) },
+        price: { required, numeric },
+        photo_url: { required, url },
+      },
+    };
+  },
   methods: {
     async onSubmit() {
-      const res = await axios.post(
-        "http://localhost:3000/apis/products/v1/products",
-        this.product
-      );
+      const isFormCorrect = await this.v$.$validate();
 
-      if (res.status == 200) {
-        this.$router.replace({ name: "Home" });
+      if (!isFormCorrect) {
+        return;
+      } else {
+        const res = await axios.post(
+          "http://localhost:3000/apis/products/v1/products",
+          this.product
+        );
+
+        if (res.status == 200) {
+          this.$router.replace({ name: "Home" });
+        }
       }
     },
   },
